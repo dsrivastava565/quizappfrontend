9bacc80eaa65b8e3a726a29c31aef4c26baeaf46
dsrivastava565 <dsrivastava565@gmail.com>
2020-07-25 18:34:36 +0530

    Updates

---
+++ precache-manifest.f88580571fbf020cc820d50f3602fc93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6844a410be50c0cbed37f1ea9039dcbe",
+    "url": "/quizappfrontend/index.html"
+  },
+  {
+    "revision": "f140fbe0aa61b677c6f2",
+    "url": "/quizappfrontend/static/css/main.e1499ad6.chunk.css"
+  },
+  {
+    "revision": "c946792da9df9fdfaaeb",
+    "url": "/quizappfrontend/static/js/2.efda7ce8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/quizappfrontend/static/js/2.efda7ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f140fbe0aa61b677c6f2",
+    "url": "/quizappfrontend/static/js/main.178b2bff.chunk.js"
+  },
+  {
+    "revision": "be1b94c59c76f10d20af",
+    "url": "/quizappfrontend/static/js/runtime-main.424d1599.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/quizappfrontend/static/media/showcase.4b31330b.jpg"
+  }
+]);
